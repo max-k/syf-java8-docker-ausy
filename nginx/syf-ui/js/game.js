a847775c96fe78add341aac47bd2f6349e945561
Nelson <nels@inthedark.com>
2017-02-15 10:32:27 +0100

    Update syf-ui REST calles to correctly communicate with the WS

--- nginx/syf-ui/js/game.js
+++ nginx/syf-ui/js/game.js
@@ -1,9 +1,22 @@
+function resetName() {
+	delete window.NAME;
+	while (!window.NAME || window.NAME.length == 0) {
+		window.NAME = prompt('Enter your name');
+		if (window.NAME) {
+			window.NAME = window.NAME.trim();
+		}
+	}
+	$('#your-name').html(window.NAME);
+	resetGame();
+	resetStats();
+}
+
 /**
  * Called to reset my game
  */
 function resetGame() {
 	function placeCardAtPosition(cardName, left, onDone) {
-		$('#my-cards .card-' + cardName).animate({
+		$('#my-cards .card-' + cardName.toLowerCase()).animate({
 			opacity: 1,
 			left: left,
 		}, 500, onDone || function() {});
@@ -13,7 +26,7 @@ function resetGame() {
 
 	placeCardAtPosition('rock', 20, function() { $(this).removeClass('selected').show(); });
 	placeCardAtPosition('paper', 200, function() { $(this).removeClass('selected').show(); });
-	placeCardAtPosition('scissors', 375, function() { $(this).removeClass('selected').show(); });
+	placeCardAtPosition('scissor', 375, function() { $(this).removeClass('selected').show(); });
 
 	// You can select only one card
 	$('#my-cards').one('click', '.card', function() {
@@ -36,20 +49,33 @@ function resetGame() {
 
 		$.ajax({
 			dataType: "json",
-			url: '/api/play',
-			data: {'card':cardName,'userName':'Nelson'},
+			/*http://localhost:8180/rps-ws/rps/playRps/Nelson/PAPER*/
+			url: '/api/rps-ws/rps/playRps/'+window.NAME+'/' + cardName,
+			data: {},
 			success: function(response) {
 				console.log('response', response);
-				showOpponentCard(response.uicard);
+				showOpponentCard(response.opponentName, response.moveOpponent);
+				showResultBox(response.result);
+				resetStats();
+				setTimeout(hideResultBox, 1500);
+			},
+			complete: function() {
 				setTimeout(resetGame, 2000);
 			}
 		});
+	});
+}
 
-/*		setTimeout(function() {
-			// show opponent card
-			var cardName = ['rock', 'paper', 'scissors'][Math.floor(Math.random()*3)];
-			showOpponentCard(cardName);
-		}, 1000);*/
+function resetStats() {
+	$.ajax({
+		dataType: "json",
+		/*http://localhost:8180/rps-ws/rps/numberOfWins/Darlys*/
+		url: '/api/rps-ws/rps/numberOfWins/'+window.NAME,
+		data: {},
+		success: function(response) {
+			console.log('response', response);
+			$('#stats-counter').html(response.numberOfWins + (response.numberOfWins > 0 ? ' victoires' : 'victoire'));
+		}
 	});
 }
 
@@ -58,14 +84,22 @@ function hideOpponentCard() {
 	$('#opponent-card .card')
 		.removeClass('card-rock')
 		.removeClass('card-paper')
-		.removeClass('card-scissors')
+		.removeClass('card-scissor')
 		.css('opacity', 0);
 }
 
-function showOpponentCard(cardName) {
+function showOpponentCard(opponentName, cardName) {
 	$('#opponent-card .card')
-		.addClass('card-' + cardName)
+		.addClass('card-' + cardName.toLowerCase())
 		.animate({
 				opacity: 1,
 			}, 500, function() {});
+}
+
+function showResultBox(result) {
+	$('#result-overlay').fadeIn();
+	$('#result').fadeIn().html('You ' + result);
+}
+function hideResultBox() {
+	$('#result-overlay, #result').fadeOut();
 }
