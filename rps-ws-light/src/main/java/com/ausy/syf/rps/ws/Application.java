b74b1fd69cf05f716a6560d70b81f12aa7f6849a
gfgk1877 <nlevert.ext@orange.com>
2017-02-22 15:24:43 +0100

    * SpringBoot on Jave EE Container

--- rps-ws-light/src/main/java/com/ausy/syf/rps/ws/Application.java
+++ rps-ws-light/src/main/java/com/ausy/syf/rps/ws/Application.java
@@ -2,14 +2,23 @@ package com.ausy.syf.rps.ws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 //@Configuration
 @SpringBootApplication
 @EnableJpaRepositories
-public class Application {
+public class Application extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(applicationClass);
+    }
+
+    private static Class<Application> applicationClass = Application.class;
 }
